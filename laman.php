a6652d9d4a241b43dc32adae6968e8187cdc6d1c
muhammadamran <amranhakim9@gmail.com>
2021-03-02 17:09:00 +0700

    Initial commit

--- laman.php
+++ laman.php
@@ -8,39 +8,31 @@ $get_api = explode('/', $url);
 if ($get_api[1] == 'api') {
 	include $get_api[1] . '/' . $get_api[2] . '.php';
 } else {
-	
+
+	include "include/laman/head.php";
 	$data = $db->query('SELECT * FROM tbl_ftheme');
 	$row = $data->fetch_assoc();
 
-	if ($row['kode']==1) {
-
+	if ($row['kode']== '1' && $row['kode']== '1') {
 		// Theme Vidio
-		include "include/laman/head.php";
 		include "pages/laman/vidio-validation.php";
-		include "include/laman/footer.php";
 
-	} else if ($row['kode']==2) {
+	} else if ($row['kode']== '2' && $row['kode']== '1') {
 
 		// Theme Slide
-		include "include/laman/head.php";
 		include "pages/laman/slide-validation.php";
-		include "include/laman/footer.php";
 
-	} else if ($row['kode']==3) {
+	} else if ($row['kode']== '3' && $row['kode']== '1') {
 
 		// Theme Single
-		include "include/laman/head.php";
 		include "pages/laman/single-validation.php";
-		include "include/laman/footer.php";
 
-	} else if ($row['kode']==4) {
+	} else if ($row['kode']== '4' && $row['kode']== '1') {
 
 		// Theme Color
-		include "include/laman/head.php";
 		include "pages/laman/color-validation.php";
-		include "include/laman/footer.php";
 
 	}
-
+	include "include/laman/footer.php";
 }
 ?>
